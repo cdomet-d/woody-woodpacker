1f13695c5c58e7d0329b291820103ca81237cf86
Charlotte Sweeting <charlottesweeting@gmail.com>
2026-08-17 15:33:09 +0200

    feat: implemented ksa

--- src/cipher.c
+++ src/cipher.c
@@ -21,26 +21,68 @@ bool create_cipher_key(unsigned char*  key) {
 /*
 	init_array_S initializes array S's 256 bytes from 0 to 255
 */
-void init_array_S(unsigned char*  S) {
-	int i = 0;
-
-	while (i < 256) {
+void init_array_S(unsigned char* S) {
+	for (int i = 0; i < 256; i++)
 		S[i] = i;
+}
+
+/*
+	swap_S_values swaps two values of a given array
+*/
+void swap_S_values(unsigned char*  S, int i , int j) {
+	char tmp = S[i];
+
+	S[i] = S[j];
+	S[j] = tmp;
+}
+
+/*
+	Key-scheduling algorithm 
+*/
+void ksa(unsigned char*  S, unsigned char*  key) {
+	int i = 0;
+	int j = 0;
+
+	init_array_S(S);
+	while (i < 256) {
+		j = (j + S[i] + key[i % 16]) % 256;
+		swap_S_values(S, i, j);
 		i++;
 	}
+}
+
+/*
+	Pseudo-random generation algorithm
+*/
+void prga(unsigned char*  S, unsigned char* text) {
+	(void)S;
+	(void)text;
+	/*
+	i := 0
+	j := 0
+	while GeneratingOutput:
+		i := (i + 1) mod 256
+		j := (j + S[i]) mod 256
+		swap values of S[i] and S[j]
+		t := (S[i] + S[j]) mod 256
+		K := S[t]
+		output K
+	endwhile
+	*/
 }
 
 bool encrypt_text(unsigned char* text) {
 	unsigned char  key[16];
 	unsigned char  S[256];
 
-	(void)text;
 	ft_bzero(key, 16);
 	ft_bzero(S, 256);
 	if (!create_cipher_key(key))
 		return false;
 	// _psuccess((char *)key);
-	init_array_S(S);
+	ksa(S, key);
+	print_S(S);
+	prga(S, text);
 	return true;
 }
 
